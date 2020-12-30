f17fbbc6ec6ee15c6141a7cfd8f00d97e65ed1ca
Guyon <guyon_cornelissen@hotmail.com>
2020-12-30 17:39:08 +0100

    initialize aws infra stack

---
+++ stack/lib/emoji-stack.ts
@@ -0,0 +1,23 @@
+import { LambdaIntegration, RestApi } from '@aws-cdk/aws-apigateway';
+import { Function as LambdaFn, Runtime, Code } from '@aws-cdk/aws-lambda';
+import { Stack, Construct, StackProps } from '@aws-cdk/core';
+
+export class EmojiStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const handler = new LambdaFn(this, 'EmojiHandler', {
+      runtime: Runtime.NODEJS_12_X,
+      code: Code.fromAsset('resources'),
+      handler: 'emoji.handle'
+    });
+
+    const api = new RestApi(this, 'EmojiApi');
+
+    const apiRoute = new LambdaIntegration(handler, {
+      requestTemplates: { "application/json": '{ "statusCode": "200" }' }
+    });
+
+    api.root.addMethod('GET', apiRoute);
+  }
+}
