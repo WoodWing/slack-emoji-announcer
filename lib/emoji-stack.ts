9db5f812ca459f5d7db93c7fe48c11a2553c5aff
Guyon Cornelissen <guyon_cornelissen@hotmail.com>
2022-02-28 14:06:06 +0100

    scout: use secret manager to set environment variables for handler instead of doing so manually (which can get overridden by deploys)

--- lib/emoji-stack.ts
+++ lib/emoji-stack.ts
@@ -1,14 +1,21 @@
 import { LambdaIntegration, RestApi } from '@aws-cdk/aws-apigateway';
 import { Function as LambdaFn, Runtime, Code } from '@aws-cdk/aws-lambda';
+import { Secret } from '@aws-cdk/aws-secretsmanager';
 import { Stack, Construct, StackProps } from '@aws-cdk/core';
 
 export class EmojiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const details = Secret.fromSecretNameV2(this, 'emoji-announcer-details', 'emoji-announcer-details');
+
     const handler = new LambdaFn(this, 'EmojiHandler', {
       runtime: Runtime.NODEJS_14_X,
       code: Code.fromAsset('dist', {}),
+      environment: {
+        SLACK_SIGNING_KEY: details.secretValueFromJson('SLACK_SIGNING_KEY').toString(),
+        SLACK_WEBHOOK_URL: details.secretValueFromJson('SLACK_WEBHOOK_URL').toString(),
+      },
       handler: 'handler.handle',
     });
 
